b96ec84b86a1fb21e94878c1e586c96ff31b11c2
Jason Liu <jaliu@tcd.ie>
2023-08-07 21:53:20 +0100

    implemented fully functioning reset

--- minesweeper.js
+++ minesweeper.js
@@ -2,13 +2,15 @@ var board = [];
 var rows = 8;
 var columns = 8;
 
-var minesCount = 8;
+var minesCount = 4;
 var minesLocation = []; // "2-2", "3-4", "2-1"
 
 var tilesClicked = 0; // goal to click all tiles except the ones containing mines
 var flagEnabled = false;
 
 var gameOver = false;
+
+var reset = false;
 
 window.onload = function() {
     startGame(); // this function is called on load of webpage due to window.onload.
@@ -33,21 +35,47 @@ function setMines() {
 function startGame() {
     document.getElementById("mines-count").innerText = minesCount; // changes the text in the element 'mines-count' to minesCount
     document.getElementById("flag-button").addEventListener("click", setFlag); // if the flag button is clicked, call setFlag function
+    document.getElementById("reset-button").addEventListener("click", resetGame);
     setMines();
 
+    
     for(let r = 0; r < rows; r++) {
         let row = [];
         for(let c = 0; c < columns; c++) {
             let tile = document.createElement("div"); // literally creates a div 
             tile.id = r.toString() + "-" + c.toString(); // sets the id of the div to the string 'rowNumber-columnNumber'
             tile.addEventListener("click", clickTile); // when tile is clicked, call clickTile function
+            if(!reset)
             document.getElementById("board").append(tile); // add tile to the board
             row.push(tile);
         }
         board.push(row);
     }
-
     console.log(board);
+}
+
+function resetGame() {
+    minesLocation = [];
+    gameOver = false;
+    flagEnabled = false;
+    tilesClicked = 0;
+    console.log("HELP");
+    resetTilesAndFlags();
+}
+
+function resetTilesAndFlags() {
+    for(let r = 0; r < rows; r++) {
+        for(let c = 0; c < columns; c++) {
+            board[r][c].style.backgroundColor = "lightgrey";
+            while(board[r][c].classList.length > 0) {
+                board[r][c].classList.remove(board[r][c].classList.item(0));
+            }
+            board[r][c].innerText = "";
+            console.log("worked");
+        }
+    }
+    reset = true;
+    startGame();
 }
 
 function setFlag() {
@@ -110,6 +138,8 @@ function checkMine(r, c) {
     }
 
     board[r][c].classList.add("tile-clicked");
+    board[r][c].style.backgroundColor = "darkgrey";
+    console.log("clicked tile in checkMine");
     tilesClicked += 1;
     let minesFound = 0; 
     
